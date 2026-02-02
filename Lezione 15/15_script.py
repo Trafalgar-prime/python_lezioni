8d723b54f43f56122d3c7c474f3993dd27e8c009
Lorenzo <tabolacci.1811191@studenti.uniroma1.it>
2026-02-03 00:18:15 +0100

    Upgrade lesson 15

--- Lezione 15/15_script.py
+++ Lezione 15/15_script.py
@@ -1,23 +1,25 @@
 def decoratore(f):
-    def wrapper():
+    def wrapper():  #wrapper non è un nome obbligatorio ma si usa per consuetudine
         print("Prima dell'esecuzione")
         f()
         print("Dopo l'esecuzione")
     return wrapper
+#praticamente wrapper avvolge la funzione facendo qualcosa prima o dopo o in entrambi i casi
 
 @decoratore #applica il decoratore alla funzione, altrimenti non vale il decoratore
+#@decoratore perche la funzione si chiama decoratore: se avesse def pluto... -> @pluto
 def saluta():
     print("ciao")
 
-saluta()
+saluta() #attivo la funzione saluta, ma anche il decoratore sulla funzione
 
 def decoratore(f):
     def wrapper(*args , **kwargs): #in questo modo creo una funzione generica che può accettare ogni parametro, quindi utile per ogni caso
-        print(f"Chiamata a {f.__name__} con argomenti : {args} , {kwargs}")
-        risultato = f(*args , **kwargs)
+        print(f"Chiamata a {f.__name__} con argomenti : {args} , {kwargs}")  #f.__name__ chiama il nome della funzione, args sta per le tuple, kwargs per i dizionari
+        risultato = f(*args , **kwargs) #è il risultato della funzione
         print(f"Risultato = {risultato}")
-        return risultato
-    return wrapper
+        return risultato #mi ritorna il risultato della funzione che ho dato in wrapper
+    return wrapper # mi ritorna dal decoratore, in modo da richiamare la funzione wrapper, con @decoratore richiamo la funzione decoratore, ma questa senza il return wrapper che mi attiva la funzione wrapper non farebbe nulla
 
 @decoratore
 def somma(a,b,**kwargs):# quando definisco la funzione in f ho gia dato tutti i possibili argomenti, quindi cosi mi chiamo solo quelli che mi servono realmente
@@ -44,6 +46,8 @@ def decoratore2(f):
 
 @decoratore1
 @decoratore2
+#in questo modo mi si incapsulano e si attiva dec1 che ha come funzione dec2 che attiva saluta, poi chiudo dec2 e poi dec1
+#non vengono lette come 2 decoratori ma come uno che attiva il secondo che attiva la funzione
 def saluta():
     print("Ciao!")
 
@@ -60,12 +64,12 @@ def decoratore(f):
 
 @decoratore
 def esempio():
-    """Questa è una funzione di esempio"""
+    """Questa è una funzione di esempio""" #questa è una docstring ovvero una linea che spiega cosa fa la funzione, nel 99% dei casi è superflua
     print("Funzione originale")
 
-print(esempio.__name__)  # ✅ Output corretto: esempio
-print(esempio.__doc__)   # ✅ Output corretto: Questa è una funzione di esempio
-esempio()
+print(esempio.__name__)  # ✅ Output corretto: esempio, che è il nome della funzione
+print(esempio.__doc__)   # ✅ Output corretto: Questa è una funzione di esempio, mi stampa la docstring della funzione
+esempio()    # così attivo la funzione che attiva il decoratore
 
 import time
 
@@ -73,7 +77,7 @@ def tempo_di_esecuzione(f):
     def wrapper(*args, **kwargs):
         inizio = time.time()
         print("Inizio tempo")
-        risultato = f(*args, **kwargs)
+        risultato = f(*args, **kwargs) #attivo la funzione quindi il time.sleep(2)
         fine = time.time()
         print("Fine tempo")
         print(f"Tempo di esecuzione: {fine - inizio:.4f} secondi")
@@ -90,7 +94,7 @@ operazione_lenta()
 
 def richiede_autenticazione(f):
     def wrapper(*args, **kwargs):
-        utente_autenticato = True  # Cambia in True per testare
+        utente_autenticato = False  # Cambia in True per testare
         if not utente_autenticato:
             print("Accesso negato!")
             return
@@ -114,7 +118,8 @@ def cache(f):
             print("Restituisco valore dalla cache")
             return memoria[args]
         risultato = f(*args)
-        memoria[args] = risultato
+        memoria[args] = risultato  #non ci va l'asterisco perchè altrimenti chiamerei l'intera tupla, invece sto generando un dizionare in cui l'args è la chiave e risultato è il valore associato
+        print("Stampo la memoria: ",memoria[args])
         return risultato
     return wrapper
 
